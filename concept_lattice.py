455b8da61c8371ca11a5bd4a8ec379cdacc3418e
Amir-Fasil <amirfasil09@gmail.com>
2025-07-02 15:02:29 +0300

    I have added the method to draw the  graph.

--- concept_lattice.py
+++ concept_lattice.py
@@ -1,3 +1,8 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from collections import defaultdict
+
+
 class ConceptLattice:
 
     def __init__(self, concepts: list):
@@ -9,21 +14,57 @@ class ConceptLattice:
         """Returns the concept lattice."""
         return self.conceptLattice
 
-    def _comp_variable(self, concept):
+    def get_proper_concept(self):
 
-        """This function is used to return the comparing variables 
-        using which the concepts are ordered. """
+        """This function retruns a list of concepts that are 
+        tuples instead of Concept objects
+        """
+        proper_concepts = []
+        for concept in self.conceptLattice:
+            proper_concepts.append(concept.get_Concept())
 
-        return len(concept.get_extent())  #This is just temporary, It will change latter
+        return proper_concepts
+    
+    def get_lattice(self):
 
-    def get_ordered_lattice(self):
+        """This function returns the concept lattice."""
+
+
+        concepts = self.get_proper_concept()
+        concept_nodes = [(frozenset(extent), frozenset(intent)) for (extent, intent) in concepts]
+
+        G = nx.Graph()
+        G.add_nodes_from(concept_nodes)
+
+
+        for i, (e1, i1) in enumerate(concept_nodes):
+            for j, (e2, i2) in enumerate(concept_nodes):
+                if i != j and i1 > i2 and len(i1) == len(i2) + 1:
+                    G.add_edge((e1, i1), (e2, i2))
+
+
+        levels = defaultdict(list)
+        for concept in concept_nodes:
+            level = len(concept[1]) 
+            levels[level].append(concept)
+
+ 
+        pos = {}
+        for level, nodes_at_level in sorted(levels.items(), reverse=True):  
+            spacing = 1.0
+            width = (len(nodes_at_level) - 1) * spacing
+            for i, node in enumerate(nodes_at_level):
+                x = i * spacing - width / 2
+                y = level
+                pos[node] = (x, y)
+
+
+        labels = {n: f"E:{set(n[0])}\nI:{set(n[1])}" for n in G.nodes()}
+        nx.draw(G, pos, labels=labels, with_labels=True, node_color='lightcoral', node_size=1800, font_size=9)
+        plt.title("Concept Lattice (Hasse Diagram, Ordered by Intent)")
+        plt.gca().invert_yaxis()  
+        plt.show()
         
-        """This function returns the ordered concept lattice."""
         
-        # Sort the concepts based on the first element of the tuple (extent)
-        ordered_concepts = sorted(self.conceptLattice, key=self._comp_variable)
-        
-        # Return the ordered list of concepts
-        return ordered_concepts
 
 
--- main.py
+++ main.py
@@ -2,15 +2,40 @@ from context import Context
 from concept_lattice import ConceptLattice
 import pandas as pd
 
-data = {
-    "A" : [1, 1, 0],
-    "B" : [1, 0, 0],
-    "C" : [0, 1, 1]
-}
+# data = {
+#     "A" : [1, 1, 0],
+#     "B" : [1, 0, 0],
+#     "C" : [0, 1, 1]
+# }
 
-dataframe = pd.DataFrame(data)
-context = Context(dataframe)
-print("Differentiation of {A, B}:", context.Differentiate({"A", "B"}))
-concepts = context.extract_concepts()
-print("Extracted Concepts:", concepts.get_concept_lattice())
-print("Ordered_lattice", concepts.get_ordered_lattice())
+# dataframe = pd.DataFrame(data)
+# context = Context(dataframe)
+# print("Differentiation of {A, B}:", context.Differentiate({"A", "B"}))
+# concepts = context.extract_concepts()
+# print("Extracted Concepts:", concepts.get_proper_concept())
+# print("Ordered_lattice", concepts.get_lattice())
+
+
+# Name : A
+# Cartoon : B
+# Real : C
+# Tortoise : D
+# Dog : E
+# Cat : F
+# Mammal : G
+
+# Garfield : 0
+# Snoopy : 1
+# Socks : 2
+# Greyfriar’s Bobby : 3
+# Harriet : 4
+
+
+dataframe2 = pd.read_csv("test_df.csv")
+dataframe2.drop(columns=dataframe2.columns[0], inplace=True)  # Drop the first unnamed column
+context2 = Context(dataframe2)
+print("Dataframe2:\n", dataframe2)
+print("Differentiation of {A, B}:", context2.Differentiate({"A", "B"}))
+concepts2 = context2.extract_concepts()
+print("Extracted Concepts:", concepts2.get_proper_concept())
+print("Ordered_lattice", concepts2.get_lattice())
